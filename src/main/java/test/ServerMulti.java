c71850a3ed843f40ff7f6808e90c08a456bae434
Florian Muller <florian.muller@etu.unice.fr>
2016-12-28 00:20:41 +0100

    first commit

---
+++ src/main/java/test/ServerMulti.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author JuIngong
+ */
+public class ServerMulti {
+
+    private ServerSocket serverSocket;
+
+    public ServerMulti(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+
+    public void launch() {
+        boolean waitClient = true;
+        try {
+            System.out.println("Server launch " + serverSocket.getLocalPort());
+            List<Thread> threads = new ArrayList<>();
+            while (waitClient) {
+                Socket socket = serverSocket.accept();
+                threads.add(new Thread(new ServiceClient(socket)));
+                threads.get(threads.size() - 1).start();
+
+            }
+            System.out.println("Server close");
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+//            int port = Integer.parseInt(args[0]);
+            int port = 8888;
+            ServerMulti serverJouet = new ServerMulti(8888);
+            serverJouet.launch();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
