7e11dee1286f3fb9ec3366603fe11377bfe3384e
Michaël Gianfaldone <m.gianfaldone@gmail.com>
2017-01-20 11:50:12 +0100

    modif

--- src/main/java/ihm/ClientInterface.java
+++ src/main/java/ihm/ClientInterface.java
@@ -2,14 +2,21 @@ package ihm;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
+
+import chord.ChordPeer;
 
 public class ClientInterface extends JFrame {
 
@@ -22,6 +29,13 @@ public class ClientInterface extends JFrame {
 	JPanel msgPanel;
 	JTextField entreeMsg;
 	JButton valSaisie;
+	
+	private String IP;
+	private int port = 0;
+	private String pseudo;
+	
+	ChordPeer peer;
+	
 
 	public ClientInterface() {
 
@@ -69,13 +83,52 @@ public class ClientInterface extends JFrame {
 		mainPanel.add(chatBoxPanel, BorderLayout.CENTER);
 		this.add(mainPanel);
 		setVisible(true);
+		
+		// - Saisie des valeurs
+		IP = JOptionPane.showInputDialog(mainPanel, "Saisir l'IP");
+		if (IP == null || IP.isEmpty()) forceClose();
+		String tmp = JOptionPane.showInputDialog(mainPanel, "Saisir n° port");
+		if (tmp == null || tmp.isEmpty() ) forceClose();
+		else {
+			port = Integer.parseInt(tmp);
+		}
+		pseudo = JOptionPane.showInputDialog(mainPanel, "Saisir le pseudo");
+		if (pseudo == null || pseudo.isEmpty()) forceClose();
+		
+		peer = new ChordPeer(pseudo, port, IP);
+		
+		System.out.println(peer.getMe().getIp() + ", " + peer.getMe().getPort() + ", " + peer.getMe().getPseudo() );
 
+		//valSaisie.setMnemonic(KeyEvent.VK_ENTER);
+		
+		valSaisie.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if ( !entreeMsg.getText().isEmpty() ) {
+				
+					String m = "[" + pseudo + "] : ";
+					m += entreeMsg.getText() + "\n";
+					
+					chatBox.append(m);
+					System.out.println("Ajout saisie");
+					entreeMsg.setText("");
+				}
+			}
+		});
+		
+	}
+	
+	private void forceClose() {
+		this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
 	}
 
 	public static void main(String[] args) {
 
 		ClientInterface client = new ClientInterface();
-
+		
+		
 	}
 
 }
