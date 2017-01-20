b76461e4925740b3619ff82f87a568e894e92093
Michaël Gianfaldone <m.gianfaldone@gmail.com>
2017-01-20 14:05:32 +0100

    modif

--- src/main/java/ihm/ClientInterface.java
+++ src/main/java/ihm/ClientInterface.java
@@ -6,6 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -32,7 +38,12 @@ public class ClientInterface extends JFrame {
 	
 	private String IP;
 	private int port = 0;
+	private int port2 = 0;
 	private String pseudo;
+	
+	final String defIP = "127.0.0.1";
+	final String defPort = "1234";
+	final String defPseudo = "Test";
 	
 	ChordPeer peer;
 	
@@ -84,21 +95,37 @@ public class ClientInterface extends JFrame {
 		this.add(mainPanel);
 		setVisible(true);
 		
+		
 		// - Saisie des valeurs
-		IP = JOptionPane.showInputDialog(mainPanel, "Saisir l'IP");
+		IP = JOptionPane.showInputDialog(mainPanel, "Saisir l'IP cible (ex: 127.0.0.1) : ", defIP);
 		if (IP == null || IP.isEmpty()) forceClose();
-		String tmp = JOptionPane.showInputDialog(mainPanel, "Saisir n° port");
+		String tmp = JOptionPane.showInputDialog(mainPanel, "Saisir votre port", defPort);
 		if (tmp == null || tmp.isEmpty() ) forceClose();
 		else {
 			port = Integer.parseInt(tmp);
 		}
-		pseudo = JOptionPane.showInputDialog(mainPanel, "Saisir le pseudo");
+		String tmp2 = JOptionPane.showInputDialog(mainPanel, "Saisir le port cible", defPort);
+		if (tmp2 == null || tmp.isEmpty() ) forceClose();
+		else {
+			port2 = Integer.parseInt(tmp);
+		}
+		pseudo = JOptionPane.showInputDialog(mainPanel, "Saisir le pseudo", defPseudo);
 		if (pseudo == null || pseudo.isEmpty()) forceClose();
 		
-		peer = new ChordPeer(pseudo, port, IP);
+		peer = new ChordPeer(pseudo, port2, IP);
 		
-		System.out.println(peer.getMe().getIp() + ", " + peer.getMe().getPort() + ", " + peer.getMe().getPseudo() );
+		System.out.println("IP : " + peer.getMe().getIp() + ", Port : " + peer.getMe().getPort() + ", Pseudo : " + peer.getMe().getPseudo() );
 
+		try {
+			Socket soc = new Socket(peer.getMe().getIp(), peer.getMe().getPort());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			//mauvaise saisie info || premier node
+			e1.printStackTrace();
+		}
+		
 		//valSaisie.setMnemonic(KeyEvent.VK_ENTER);
 		
 		valSaisie.addActionListener(new ActionListener() {
@@ -111,6 +138,8 @@ public class ClientInterface extends JFrame {
 					String m = "[" + pseudo + "] : ";
 					m += entreeMsg.getText() + "\n";
 					
+					Map message = preparerMessage(m);
+					
 					chatBox.append(m);
 					System.out.println("Ajout saisie");
 					entreeMsg.setText("");
@@ -118,6 +147,16 @@ public class ClientInterface extends JFrame {
 			}
 		});
 		
+	}
+	
+	//Generation message à envoyer
+	private Map<String, String> preparerMessage(String m) {
+		 Map<String, String> msg = new HashMap<>();
+	        msg.put("type", "msg");
+	        msg.put("exp", "" + peer.getMe().getId());
+	        msg.put("content", m);
+	        
+	      return msg;  
 	}
 	
 	private void forceClose() {
